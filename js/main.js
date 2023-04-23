41bb0990b0b0584a6e5207e6e1740069f0814ba9
fgobron <fgobron@free.fr>
2023-04-23 22:29:43 +0200

    feat(styles): add art manga

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 (function() {
     const videoElement = document.getElementById('video-player');
+    const buttonPlay = document.getElementById('play-button');
 
     window.togglePlay = function() {
       if (videoElement.paused) {
         videoElement.play();
+        buttonPlay.style.display = 'none';
       } else {
         videoElement.pause();
+        buttonPlay.style.display = 'block';
       }
     };
   })();
