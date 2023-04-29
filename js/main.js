459f65ed810b20870b1328a0185a11d060a87810
fgobron <fgobron@free.fr>
2023-04-29 18:39:44 +0200

    feat(styles): cleaning and menu

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
 (function() {
-    const videoElement = document.getElementById('video-player');
-    const buttonPlay = document.getElementById('play-button');
-
-    window.togglePlay = function() {
-      if (videoElement.paused) {
-        videoElement.play();
-        buttonPlay.style.display = 'none';
-      } else {
-        videoElement.pause();
-        buttonPlay.style.display = 'block';
-      }
-    };
-  })();
+  // Your code here...
+}());
