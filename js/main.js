91fbe64791cd0886c3b1853aa4a31ed320f295c6
fgobron <fgobron@free.fr>
2023-06-05 03:24:53 +0200

    feat(responsive): add navbar

--- js/main.js
+++ js/main.js
@@ -1,8 +1,21 @@
 document.addEventListener("DOMContentLoaded", function () {
   const menuBurger = document.querySelector(".menu-burger");
   const navbar = document.querySelector("#navbar");
+  const links = navbar.querySelectorAll("a");
 
-  menuBurger.addEventListener("click", () => {
+  menuBurger.addEventListener("click", (e) => {
+    e.stopPropagation();
     navbar.classList.toggle("active");
+  });
+
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.stopPropagation();
+      navbar.classList.remove("active");
+    });
+  });
+
+  document.addEventListener("click", () => {
+    navbar.classList.remove("active");
   });
 });
